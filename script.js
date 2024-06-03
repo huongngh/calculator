aa7d3eafd63c81da2bdddac0381655a85761b1ab
huonghuynh <e.huonghuynh@gmail.com>
2024-06-03 11:13:28 +0700

    Create operate function in script

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ const divide = function(a, b){
     }
     return a/b;
 }
+
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+
+const operate = function(operator, firstNumber, secondNumber){
+    if (operator === '+'){
+        return add(firstNumber, secondNumber);
+    }
+    else if (operator === '-'){
+        return subtract(firstNumber, secondNumber);
+    }
+    else if (operator === '*'){
+        return multiply(firstNumber, secondNumber);
+    }
+    else if (operator === '/'){
+        return divide(firstNumber, secondNumber);
+    }
+}
