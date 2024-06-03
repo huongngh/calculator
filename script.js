901b1ced8478b1f51225311a0e403f6f4318b2bc
huonghuynh <e.huonghuynh@gmail.com>
2024-06-03 16:18:41 +0700

    Store all the values and call the operate function with them in script

--- script.js
+++ script.js
@@ -1,62 +1,101 @@
 const add = function(a, b){
-    return a + b;
+    return parseFloat(a) + parseFloat(b);
 }
 const subtract = function(a, b){
-    return a - b;
+    return parseFloat(a) - parseFloat(b);
 }
 const multiply = function(a, b){
-    return a * b;
+    return parseFloat(a) * parseFloat(b);
 }
 const divide = function(a, b){
-    if (b === 0){
+    if (b === '0'){
         return NaN;
     }
-    return a/b;
+    return parseFloat(a)/parseFloat(b);
 }
 
+let displayValue = '0';
 let firstNumber = '';
 let secondNumber = '';
 let operator = '';
 
-const operate = function(operator, firstNumber, secondNumber){
-    if (operator === '+'){
-        return add(firstNumber, secondNumber);
+
+const operate = function(){
+    let result;
+    switch(operator){
+        case ('+'):
+            result = add(firstNumber, secondNumber); 
+            break;
+        case ('-'):
+            result = subtract(firstNumber, secondNumber);
+            break;
+        case ('*'):
+            result = multiply(firstNumber, secondNumber);
+            break;
+        case ('/'):
+            result = divide(firstNumber, secondNumber);
+            break;
+        default:
+            result = 'Error';
     }
-    else if (operator === '-'){
-        return subtract(firstNumber, secondNumber);
-    }
-    else if (operator === '*'){
-        return multiply(firstNumber, secondNumber);
-    }
-    else if (operator === '/'){
-        return divide(firstNumber, secondNumber);
-    }
+    displayValue = result;
+    updateDisplay();
 }
 
-let displayValue = '';
-
-const display = document.querySelector('.display');
-
-const digitContainer = document.querySelector('.digit');
-const buttonValues = digitContainer.querySelectorAll('button');
-
-buttonValues.forEach(button => {
-    button.addEventListener('click',()=>{
-        let number = button.textContent;
-        addToDisplay(number);
-        console.log(number);
-    })
-})
-
-const addToDisplay = function(number){
-    if (number === 0){
-        displayValue = number;
-    }
-    else{
-        displayValue += number;
+const clear = function(){
+    displayValue = displayValue.slice(0, -1);
+    if (displayValue === ''){
+        displayValue = 0;
     }
     updateDisplay();
 }
+const reset = function(){
+    displayValue = '0';
+    firstNumber = '';
+    secondNumber = '';
+    operator = '';
+    updateDisplay();
+}
+
+
+const buttonContainer = document.querySelector('.container');
+const buttons = buttonContainer.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click',() => {
+        let currentValue = button.textContent;
+        if (!isNaN(currentValue)){
+            if (operator === ''){
+                firstNumber += currentValue;
+                displayValue = firstNumber;
+                console.log(firstNumber);
+            }
+            else{
+                secondNumber += currentValue;
+                displayValue = secondNumber;    
+                console.log(secondNumber);
+            }
+        }
+        else if (currentValue === '='){
+            operate();
+        }
+        else if (currentValue === 'C'){
+            clear();
+        }
+        else if (currentValue === 'AC'){
+            reset();
+        }
+        else {  
+            operator = currentValue;
+            displayValue = operator;   
+            console.log(operator);
+        }
+        updateDisplay();
+    })
+})
+
 const updateDisplay = function(){
+    const display = document.querySelector('.display');
     display.textContent = displayValue;
 }
+
